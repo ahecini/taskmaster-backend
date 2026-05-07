84e1ebfc2587a706fa60ca4407a8e937aa7bc250
ahecini <hecini.abdeldjalil@gmail.com>
2026-05-07 13:42:23 +0200

    Allowing the frontend to connect to the backend

--- src/main/java/Payroll/TaskFolder/TaskController.java
+++ src/main/java/Payroll/TaskFolder/TaskController.java
@@ -19,9 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class TaskController {
 
